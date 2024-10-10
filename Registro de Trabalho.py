fa76b29b724829d2d22daf109f4982ba24e13176
Wolf99912 <46465407+Wolf99912@users.noreply.github.com>
2024-10-10 20:41:29 -0300

    Update Registro de Trabalho.py

--- Registro de Trabalho.py
+++ Registro de Trabalho.py
@@ -4,18 +4,23 @@ class Registro:
         self.carga = carga
         self.valor = valor
 
+    def calcular_salario(self):
+        return self.carga * self.valor
+
 def coletar_dados():
     while True:
-
         nome = input("Coloque seu nome: ")
-        carga = int(input("Coloque sua carga horária: "))
+        carga = int(input("Coloque sua carga horária: "))  
         valor = float(input("Coloque o valor do horário: "))
-        finalizar = input("Deseja finalizar? S/N ").upper()
+
+        finalizar = input("Deseja finalizar? S/N ").upper()  
         if finalizar == 'S':
             break
+    
     registro = Registro(nome, carga, valor)
     
-    print(f"\nNome: {registro.nome}, Carga Horária: {registro.carga}, Valor: {registro.valor}")
-    
+    print(f"\nNome: {registro.nome}, Carga Horária: {registro.carga}, Valor por hora: {registro.valor}")
+    print(f"Salário calculado: {registro.calcular_salario()}")      
     return registro
+
 registro_criado = coletar_dados()
